2780246310e9fabe3e9637e122cb0694d097c39f
Fabiano <fabipiroli@live.com>
2025-06-01 17:01:59 -0300

    update

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DankiCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nomes;
+            nomes = new string[5];
+
+            // Para acessar o Nome é preciso informar o índice do array, ou seja o endereço do array.
+            nomes[0] = "Joana"; 
+            nomes[1] = "Roberto";
+            nomes[2] = "João";
+            nomes[3] = "Natália";
+            nomes[4] = "Ana";
+
+            // Para exibir o conteúdo do array, é necessário percorrer o array com um loop.
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+        }
+    }
+}
